ef01b19bc40b0691e061cb53342a066a66b4aba0
chadd williams <will4614@pacificu.edu>
2022-09-04 10:54:54 -0700

    Add project files.

---
+++ GitHubVSTest/main.cpp
@@ -0,0 +1,29 @@
+//*********************************************************************
+// File name:			main.cpp
+// Author:				chadd williams
+// Date:					Sep 4, 2022
+// Class:					CS 150-02
+// Assignment:		In Class Practice
+// Purpose:				Demonstrate GitHub and VS 
+// Hours Worked:	.1
+//*********************************************************************
+#include <iostream>
+
+using namespace std;
+
+//*********************************************************************
+// Function:		main
+//
+// Description: Say Hello!
+//
+// Parameters:	None
+//
+// Returned:		EXIT_SUCCESS
+//*********************************************************************
+int main() {
+
+	cout << " HELLO FROM GITHUB AND VS " << endl;
+
+	return EXIT_SUCCESS;
+}
+
